1d706249a3e302b1d85080b468d3eaaa9164f589
github username <github email address>
2019-10-07 14:19:54 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+let text = document.getElementById('text');
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    text.innerHTML = "This is really cool!"
+  });
+   
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+    );
